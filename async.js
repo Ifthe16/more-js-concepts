26e948d7b979bfd227ef83171911eccaad9f4164
Ifthe16 <habiburrahmanif975@gmail.com>
2024-10-06 01:23:35 +0600

    added async files

---
+++ async.js
@@ -0,0 +1,17 @@
+console.log(1);
+console.log(2);
+// setTimeout(doSomething);
+/* 
+setTimeout(doSomething, 4000); //4 sec = 4000 millisecond 
+*/
+setTimeout(() => {
+    console.log('lazy logged');
+}, 4000);
+console.log(4);
+console.log(5);
+console.log(6);
+
+function doSomething() {
+    console.log(3);
+}
+// sequency 
