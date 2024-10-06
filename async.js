b771c83a9e4545170a94b1c6c1b6bb1ce548f888
Ifthe16 <habiburrahmanif975@gmail.com>
2024-10-06 09:53:12 +0600

    added setTimeout file and modified async files

--- async.js
+++ async.js
@@ -1,17 +1,35 @@
-console.log(1);
-console.log(2);
-// setTimeout(doSomething);
-/* 
-setTimeout(doSomething, 4000); //4 sec = 4000 millisecond 
-*/
-setTimeout(() => {
-    console.log('lazy logged');
-}, 4000);
-console.log(4);
-console.log(5);
-console.log(6);
-
-function doSomething() {
-    console.log(3);
+const myLoader = () => {
+    return new Promise((resolve, reject) => {
+        // const success = true;
+        const success = Math.random();
+        if (success <= 0.5) {
+            resolve(success);
+        }
+        else {
+            reject(success);
+        }
+    })
 }
-// sequency 
+
+myLoader()
+    .then(data => console.log('resolved data', data))
+    .catch(err => console.log('rejected with value', err))
+
+fetch('')
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.error(err))
+
+async function loadData() {
+    const res = await fetch('')
+    const data = await res.json();
+    console.log(data);
+}
+
+loadData();
+
+const taskLoader = async () => {
+    const res = await fetch('');
+}
+
+taskLoader();
