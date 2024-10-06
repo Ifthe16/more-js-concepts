b772c86b3a1707741ffe098d545db37d98535f20
Ifthe16 <habiburrahmanif975@gmail.com>
2024-10-06 21:20:09 +0600

    added promise file and modified async file of js

--- async.js
+++ async.js
@@ -1,20 +1,3 @@
-const myLoader = () => {
-    return new Promise((resolve, reject) => {
-        // const success = true;
-        const success = Math.random();
-        if (success <= 0.5) {
-            resolve(success);
-        }
-        else {
-            reject(success);
-        }
-    })
-}
-
-myLoader()
-    .then(data => console.log('resolved data', data))
-    .catch(err => console.log('rejected with value', err))
-
 fetch('')
     .then(res => res.json())
     .then(data => console.log(data))
